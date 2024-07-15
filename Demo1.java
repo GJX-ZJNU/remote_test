20e98b7027f77b740b3abd44b07e804138fdb363
GJX_ZJNU <18116077762@163.com>
2024-07-15 15:10:00 +0800

    Create Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1{
+	public static void main(String[] args){
+		System.out.println("北京\t天津\t上海");
+		System.out.println("GJX\nLYY\nSYH");
+		System.out.println("\\\\Mac\\Home\\Documents\\JAVA>");
+		System.out.println("我说：\"好好学\"");
+		System.out.println("北京你好\r上海");//输出结果 上海你好
+	}
+}
